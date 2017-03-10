2f55babf01227e197152a8823fa440ec2760411b
lmingxing <2992022660@qq.com>
2017-03-10 17:27:29 +0800

    添加fragment与其布局文件

---
+++ app/src/main/java/com/best/limingxing/best/fragment/FaXianFragment.java
@@ -0,0 +1,30 @@
+package com.best.limingxing.best.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.best.limingxing.best.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FaXianFragment extends Fragment {
+
+
+    public FaXianFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fa_xian, container, false);
+    }
+
+}
