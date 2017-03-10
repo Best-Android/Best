86324e2c85209053edaff7b3b6fb7678ecbaad7e
lmingxing <2992022660@qq.com>
2017-03-10 17:28:26 +0800

    添加BaseActivity类

---
+++ app/src/main/java/com/best/limingxing/best/activity/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.best.limingxing.best.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+        initData();
+        setListener();
+    }
+
+    protected abstract void initView();
+    protected abstract void initData();
+    protected abstract void setListener();
+}
